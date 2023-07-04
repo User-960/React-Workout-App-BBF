00cab858a541ae6dd54c0228b9e4afd569ba687f
Ivan <ivanvolodin960@gmail.com>
2023-07-05 01:18:50 +0300

    feat: create statistics

--- app/user/user.routes.js
+++ app/user/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
-import { getUserProfile } from './user-controller.js'
+import { getUserProfile } from './user.controller.js'
 
 const router = express.Router()
 
--- server.js
+++ server.js
@@ -7,9 +7,9 @@ import path from 'path'
 import { errorHandler, notFound } from './app/middleware/error.middleware.js'
 
 import authRoutes from './app/auth/auth.routes.js'
-import userRoutes from './app/exercise/exercise.routes.js'
 import exerciseRoutes from './app/exercise/exercise.routes.js'
 import { prisma } from './app/prisma.js'
+import userRoutes from './app/user/user.routes.js'
 import workoutRoutes from './app/workout/workout.routes.js'
 
 const app = express()
